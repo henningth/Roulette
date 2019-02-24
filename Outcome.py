e79a1a732e29f56128cce68d268f2d5be9935379
Henning Thomsen <henningthomsen@protonmail.com>
2019-02-24 16:10:08 +0100

    First definition of Outcome class

---
+++ Outcome.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 15:54:11 2019
+
+Outcome class
+
+@author: HTH
+"""
+
+class Outcome:
+    
+    # Initializer
+    def __init__(self, name, odds):
+        self.name = name
+        self.odds = odds
