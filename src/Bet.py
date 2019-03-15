304ade720e8915ce8874e2b880d116c563f130d5
Henning Thomsen <ht1703@gmail.com>
2019-03-15 14:21:34 +0100

    Added Bet and Game classes

---
+++ src/Bet.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 14:20:58 2019
+
+Bet class
+
+@author: HTH
+"""
+
+class Bet:
+    
+    pass
---
+++ src/Game.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 20:03:29 2019
+
+Game class
+
+@author: HTH
+"""
+
+from src.Bin import Bin
+from src.Outcome import Outcome
+from src.Wheel import Wheel
+
+class Game:
+    
+    self.wheel = Wheel()
+    self.table = Table()
+    self.player = Player()
+
+if __name__ == "__main__":
+    
+    Bin()
+    Outcome("Number 00", 36)
+    Wheel()
