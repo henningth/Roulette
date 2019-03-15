ab7d975ab3354defe715f618d5eed281a1134c96
Henning Thomsen <ht1703@gmail.com>
2019-03-15 14:39:19 +0100

    First version of Game class

---
+++ src/Game.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 14:38:46 2019
+
+@author: HTH
+"""
+
+from src.Bin import Bin
+from src.Outcome import Outcome
+from src.Wheel import Wheel
+
+class Game:
+    
+    self.wheel = Wheel()
+    self.table = Table()
+    self.player = Player()
+
+if __name__ == "__main__":
+    
+    Bin()
+    Outcome("Number 00", 36)
+    Wheel()
